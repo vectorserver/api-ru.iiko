aa6695610e6a054314f1372f2674faa07914aa49
vectorserver <vectorserver@mail.ru>
2022-11-10 12:57:52 +0500

    first

--- test.php
+++ test.php
@@ -4,12 +4,8 @@ $iiko = new \iikoCloud\iikoCloud('928b504e-f04');
 
 $organizations = $iiko->getOrganizations();
 
-
-
 $org_id = $organizations[0]['id'];
 
-
 //$stop_lists = $iiko->getStopLists($org_id);
-
 $menu = $iiko->getMenu($org_id);
 var_dump($menu);
